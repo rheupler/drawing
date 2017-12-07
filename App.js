4f014515e4e6383815fab349de970ff30b7274ae
ryan heupler <rheupler@gmail.com>
2017-12-07 02:04:24 -0500

    playing with gradients

--- App.js
+++ App.js
@@ -2,20 +2,36 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { LinearGradient, BlurView, GLView } from 'expo';
 
+let colors = ['#9c44f2', '#726ef8', '#4fcef9'];
+
+
+
 export default class App extends React.Component {
   _onPressButton() {
     alert("Tapped Button")
   }
 
+  _animate() {
+    let pos = 0;
+    let id = setInterval(frame, 5)
+    function frame() {
+      if (pos === 400) {
+        clearInterval(this)
+      } else {
+        pos++;
+        styles.button.top = `${pos}px`
+      }
+    }
+  }
+
+
   render() {
     return (
       <LinearGradient
-      colors={['#9c44f9', '#726ef8', '#4fcef9']}
+      colors={colors}
       style={styles.container}
       >
-
-      
-
+        <Button className="button" style={styles.button} title={"YO"} onPress={this._animate.bind(this)}></Button>
       </LinearGradient>
     );
   }
@@ -25,8 +41,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightgray',
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative'
   },
+  button: {
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'red',
+    position: 'absolute'
+  }
 });
